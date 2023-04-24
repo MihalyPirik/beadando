3fb42d6a4c02a58db178b2dfc1d0857444116c2e
Pinter Armin <pinter.armin@students.jedlik.eu>
2023-04-24 08:49:11 +0200

    main.js üres volt

--- beadando/main.js
+++ beadando/main.js
@@ -17,3 +17,22 @@ setInterval(function()
     OutsideTheBoard()
     PoisonEffect()
 },speed)
+import { FoodEffect, OutsideTheBoard, PoisonEffect, SnakeBodyCollide, Win } from "./interactions.js";
+import { SetBoard,speed } from "./board.js";
+import { PlaceFood,PlacePoison,PlaceSnakeHead,SnakeMove,PoisonMove } from "./objects.js";
+SetBoard()
+PlaceFood()
+PlacePoison()
+PlaceSnakeHead()
+
+
+setInterval(function()
+{
+    Win()
+    SnakeMove()
+    // PoisonMove()
+    FoodEffect()
+    SnakeBodyCollide()
+    OutsideTheBoard()
+    PoisonEffect()
+},speed)
